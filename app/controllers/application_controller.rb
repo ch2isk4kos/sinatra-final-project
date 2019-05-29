ee2b2a95997762c9d550f141201477cf6073ef50
ch2isk4kos <ch2isk4kos@gmail.com>
2019-05-28 22:32:06 -0400

    added and configured rack-flash gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 require './config/environment'
+require 'rack-flash'
+require 'sinatra/base'
 
 class ApplicationController < Sinatra::Base
 
     configure do
       set :public_folder, 'public'
       set :views, 'app/views'
+      enable :sessions
+      set :session_secret, "dead_in_the_middle_of_little_italy"
+      use Rack::Flash
     end
 
     get "/" do
