61c09aa0b63a8803c27f1063a24b6e1b2c4b2b33
ch2isk4kos <ch2isk4kos@gmail.com>
2019-05-28 20:46:10 -0400

    created and migrated users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    validates :email, presence: true
+    validates :password, presence: true
+    validates_uniqueness_of :email
+    validates_uniqueness_of :username
 end
