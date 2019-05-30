bc2529624e084669f0630c47c6a38bf613bdc715
ch2isk4kos <ch2isk4kos@gmail.com>
2019-05-29 20:00:21 -0400

    added user sign up and login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
 
     #INDEX
     # get "/users" do
-    #   erb :"/users/index.html"
+    #   @users = User.all.order("created_at DESC")
+    #   erb :"/users/index"
     # end
-
 
     #NEW
     get "/signup" do
@@ -13,9 +13,17 @@ class UsersController < ApplicationController
 
     #CREATE
     post "/signup" do
-      redirect "/users/login"
-    end
+      user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
+      if !user.save
+        flash[:message] = "What Did You Do? Is Everything OK? Let's Try Again."
+        erb :'users/new'
+      else
+        flash[:message] = "You Have Successfully Signed Up"
+        session[:user_id] = user.id
+        redirect "/login"
+      end
+    end
 
     #LOGIN
     get '/login' do
@@ -23,29 +31,53 @@ class UsersController < ApplicationController
     end
 
     post "/login" do
-      redirect "/users/#{@user.id}"
+      user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          flash[:message] = "You Are Logged In"
+          redirect '/'
+        else
+          flash[:message] = "Something Went Completely Wrong."
+          erb :"users/login"
+        end
     end
 
+    #LOGOUT
+    get '/logout' do
+      if logged_in?
+        session.destroy
+        flash[:message] = "You're Signed Out"
+        redirect '/'
+      end
+    end
 
     #SHOW
     get "/users/:id" do
+      @user = User.find_by_id(params[:id])
       erb :"users/show"
     end
 
-
     #EDIT
     get "/users/:id/edit" do
+      @user = User.find_by_id(params[:id])
       erb :"users/edit"
     end
 
     #UPDATE
     patch "/users/:id" do
-      redirect "/users/:id"
+      @user = User.find_by_id(params[:id])
+      @user.username = params[:username]
+      @user.email = params[:email]
+      @user.password = params[:password]
+      @user.save!
+      flash[:message] = "You've Updated Your Profile"
+      redirect "/users/#{@user.id}"
     end
-
 
     #DESTROY
     delete "/users/:id/delete" do
+      @user = User.find_by_id(params[:id])
+      flash[:message] = "That's Fucked Up! I Didn't Want You Here Anyway!"
       redirect "/users"
     end
 end
