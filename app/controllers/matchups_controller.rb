f82147dddf000928003947b55d087bc7db070187
ch2isk4kos <ch2isk4kos@gmail.com>
2019-05-30 21:13:00 -0400

    added index, new, create and show action functionality for a matchup && forms and views

--- app/controllers/matchups_controller.rb
+++ app/controllers/matchups_controller.rb
@@ -1,37 +1,75 @@
 class MatchupsController < ApplicationController
 
-  # GET: /matchups
-  get "/matchups" do
-    erb :"/matchups/index.html"
-  end
+    #INDEX
+    get "/matchups" do
+      if !logged_in?
+        flash[:message] = "You Have to Login to Do That."
+        redirect "/login"
+      else
+        @matchups = Matchup.all.order("created_at DESC")
+        erb :"matchups/index"
+      end
+    end
 
-  # GET: /matchups/new
-  get "/matchups/new" do
-    erb :"/matchups/new.html"
-  end
+    #NEW
+    get "/matchups/new" do
+      if !logged_in?
+        flash[:message] = "You Have to Login to Do That."
+        redirect "/login"
+      else
+        erb :"matchups/new"
+      end
+    end
 
-  # POST: /matchups
-  post "/matchups" do
-    redirect "/matchups"
-  end
+    #CREATE
+    post "/matchups" do
+      if !logged_in?
+        flash[:message] = "You Have to Login to Do That."
+        redirect "/login"
+      else
+        if params.values == ""
+          flash[:message] = "What Did You Do? Are You OK? Try Again."
+          erb :"matchups/new"
+        else
+          @matchup = Matchup.create(
+            opp_name: params[:opp_name],
+            team: params[:team],
+            opp_team: params[:opp_team],
+            score: params[:score],
+            opp_score: params[:opp_score],
+            system: params[:system],
+            description: params[:description],
+            user_id: "#{current_user.id}"
+          )
+          flash[:message] = "Game On! New Matchup Posted."
+          redirect "/matchups/#{@matchup.id}"
+        end
+      end
+    end
 
-  # GET: /matchups/5
-  get "/matchups/:id" do
-    erb :"/matchups/show.html"
-  end
+    #SHOW
+    get "/matchups/:id" do
+        if !logged_in?
+          flash[:message] = "You Have to Login to Do That."
+          redirect "/login"
+        else
+          @matchup = Matchup.find_by_id(params[:id])
+          erb :"matchups/show"
+        end
+    end
 
-  # GET: /matchups/5/edit
-  get "/matchups/:id/edit" do
-    erb :"/matchups/edit.html"
-  end
+    #EDIT
+    get "/matchups/:id/edit" do
+      erb :"matchups/edit"
+    end
 
-  # PATCH: /matchups/5
-  patch "/matchups/:id" do
-    redirect "/matchups/:id"
-  end
+    #UPDATE
+    patch "/matchups/:id" do
+      redirect "/matchups/:id"
+    end
 
-  # DELETE: /matchups/5/delete
-  delete "/matchups/:id/delete" do
-    redirect "/matchups"
-  end
+    #DELETE
+    delete "/matchups/:id/delete" do
+      redirect "/matchups"
+    end
 end
